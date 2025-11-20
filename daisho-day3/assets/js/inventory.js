107ca80db78882ad41b3d14bc6b51adf90b805f4
hiraizan0808-star <hiraizan0808@gmail.com>
2025-11-20 15:03:02 +0900

    20251120

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -1,24 +1,70 @@
-const items =[
-    {
-        id:"D001",
-        name:"オレンジジュース",
-        price:150,
-        stock:20,
-        minStock:5
-    },
-    {
-        id:"D002",
-        name:"コーラ",
-        price:150,
-        stock:12,
-        minStock:5
-    },
-    {
-        id:"D003",
-        name:"お茶",
-        price:120,
-        stock:30,
-        minStock:10
-    }
+
+//--------------------
+//1.在庫データ（JSON的な構造）
+//--------------------
+const items = [
+    {id:"D001",name:"オレンジジュース",price:150,stock:20,minStock:5},
+    {id:"D002",name:"コーラ",         price:150,stock:12,minStock:5},
+    {id:"D003",name:"お茶",           price:120,stock:30,minStock:10}
 ];
 
+//HTMLで <tbody id ="item-list">と書いた部分をJavascriptから取得する
+const tbody = document.getElementById("item-list");
+
+
+//-------------------
+//2.表示用の関数　render()
+//-------------------
+function render(){
+    //一度、中身を空にしてから作り直す
+    tbody.innerHTML="";
+
+    //itemsの中身を1件ずつ取り出してtr(行)を作る
+    items.forEach(item =>{
+        const tr = document.createElement("tr");
+    
+    //在庫が少ないときはクラスを追加して背景色を変える
+    if(item.Stock<item.minStock){
+        tr.classList.add("low-stock");
+    }
+
+    //行の中身(セル）をテンプレート文字列でまとめて書く
+    tr.innerHTML=`
+    <td>${item.id}</id>
+    <td>${item.name}</id>
+    <td>${item.price}</id>
+    <td>${item.stock}</id>
+    <td>
+       <button onclick="changeStock('${item.id}',1)">+</button>
+       <button onclick="changeStock('${item.id}',-1)">-</button>
+    </id>
+    `;
+
+    //つくったtrをtbodyの中に追加する
+    tbody.appendChild(tr);
+    });
+}
+
+//-------------------
+//3.在庫を変更する関数　changeStock()
+//-------------------
+function changeStock(id,diff){
+    //idが一致する商品を探す
+    const item=items.find(i=>i.id===id);
+    if(!item){
+        return;//見つからなかったら何もしない
+    }
+
+    //diff分だけ在庫数を増減させる
+    item.stock+=diff;
+    //在庫がマイナスにならないように０で止める
+    if(item.stock<0){
+        item.stock=0;
+    }
+
+    //変更を画面に反映する
+    render();
+}
+
+//ページを開いたときに、１回だけ表示する
+render();
